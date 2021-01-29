020a6e2d84951e6be3a1ae517036f731ff4252d6
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-01-30 00:11:27 +0300

    1

---
+++ Tennis/Match.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennis
+{
+    class Match
+    {
+
+        Player1 playerOne = new Player1();
+        Player2 playerTwo = new Player2();
+
+        public int Game()
+        {
+            int _playerOneServe = playerOne.Serve();
+            int _playerTwoServe = playerTwo.Serve();
+
+            if (_playerOneServe == _playerTwoServe)
+            {
+               
+            }
+
+            playerOne.WonServes();
+        }
+
+    }
+        
+    
+}
---
+++ Tennis/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennis
+{
+    abstract class Player
+    {
+        public abstract int WonServes(int value);
+
+        public abstract int WonSets(int value);
+
+        public abstract int WonGames(int value);
+    }
+}
